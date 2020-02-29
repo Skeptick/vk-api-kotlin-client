57fad14708d70e0c1d58d08a9ed52512ec1b3bee
Skeptick <skeptick13@gmail.com>
2020-02-29 21:57:58 +0500

    Setup build.gradle.kts for js and native artifacts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,20 +1,14 @@
-import com.jfrog.bintray.gradle.BintrayExtension
-import com.jfrog.bintray.gradle.BintrayPlugin
-import org.jetbrains.kotlin.gradle.plugin.KotlinPlatformJvmPlugin
-import org.jetbrains.kotlinx.serialization.gradle.SerializationGradleSubplugin
-
 buildscript {
     repositories {
         jcenter()
         mavenCentral()
     }
 
-    val kotlinVersion: String by extra
-    val bintrayVersion: String by extra
+    val kotlinVersion = "1.3.61"
+
     dependencies {
         classpath(kotlin("gradle-plugin", kotlinVersion))
         classpath(kotlin("serialization", kotlinVersion))
-        classpath("com.jfrog.bintray.gradle:gradle-bintray-plugin:$bintrayVersion")
     }
 }
 
@@ -22,63 +16,5 @@ allprojects {
     repositories {
         jcenter()
         mavenCentral()
-        maven("https://kotlin.bintray.com/kotlinx")
-        maven("https://kotlin.bintray.com/ktor")
-    }
-}
-
-subprojects {
-    apply<KotlinPlatformJvmPlugin>()
-    apply<JavaLibraryPlugin>()
-    apply<SerializationGradleSubplugin>()
-    apply<MavenPublishPlugin>()
-    apply<BintrayPlugin>()
-
-    val artifactPublish: String by extra
-    val artifactGroupId: String by extra
-    version = artifactPublish
-    group = artifactGroupId
-
-    configure<PublishingExtension> {
-        val sourceSets: SourceSetContainer by project
-        val sourcesJar by tasks.registering(Jar::class) {
-            from(sourceSets["main"].allSource)
-            archiveClassifier.set("sources")
-        }
-
-        publications {
-            register(project.name, MavenPublication::class) {
-                from(components["java"])
-                artifact(sourcesJar.get())
-                groupId = artifactGroupId
-                artifactId = project.name
-                version = artifactPublish
-
-                pom {
-                    licenses {
-                        license {
-                            name.set("Apache-2.0")
-                            url.set("https://opensource.org/licenses/Apache-2.0")
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-    configure<BintrayExtension> {
-        val bintrayUser: String? by project
-        val bintrayKey: String? by project
-        
-        user = bintrayUser
-        key = bintrayKey
-        setPublications(project.name)
-        publish = true
-
-        pkg.apply {
-            repo = "maven"
-            name = project.name
-            version.apply { name = artifactPublish }
-        }
     }
 }
--- vk-api-kotlin-client/build.gradle.kts
+++ vk-api-kotlin-client/build.gradle.kts
@@ -1,15 +1,82 @@
-sourceSets {
-    main { java.srcDir("src/main/kotlin") }
-    test { java.srcDir("src/test/kotlin") }
+plugins {
+    kotlin("multiplatform")
+    kotlin("plugin.serialization")
+    id("org.gradle.maven-publish")
 }
 
-dependencies {
-    val serializationVersion: String by extra
-    val coroutinesVersion: String by extra
-    val ktorVersion: String by extra
+group = "tk.skeptick"
+version = "0.2.1"
 
-    api(kotlin("stdlib"))
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-runtime:$serializationVersion")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion")
-    api("io.ktor:ktor-client:$ktorVersion")
+kotlin {
+    val ideaActive = System.getProperty("idea.active") == "true"
+    val os = org.gradle.internal.os.OperatingSystem.current()!!
+
+    if (ideaActive) {
+        jvm()
+        js().nodejs()
+        if (os.isWindows) mingwX64()
+        if (os.isLinux) linuxX64()
+        if (os.isMacOsX) macosX64()
+    } else if (os.isMacOsX) {
+        jvm()
+        js().nodejs()
+        linuxX64()
+        macosX64()
+        iosArm32()
+        iosArm64()
+        iosX64()
+        tvosArm64()
+        tvosX64()
+    } else if (os.isWindows) {
+        mingwX64()
+    }
+
+    sourceSets {
+        commonMain {
+            kotlin.srcDir("src")
+            dependencies {
+                api(kotlin("stdlib"))
+                implementation("org.jetbrains.kotlinx:kotlinx-serialization-runtime-native:0.14.0")
+                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-native:1.3.3")
+                api("io.ktor:ktor-client-core-native:1.3.1")
+            }
+        }
+    }
+}
+
+publishing {
+    publications.withType<MavenPublication>().all {
+        pom {
+            name.set("vk-api-kotlin-client")
+            url.set("https://github.com/Skeptick/vk-api-kotlin-client")
+            licenses {
+                license {
+                    name.set("The Apache Software License, Version 2.0")
+                    url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                }
+            }
+            developers {
+                developer {
+                    name.set("Danil Yudov")
+                    email.set("skeptick13@gmail.com")
+                }
+            }
+            scm {
+                connection.set("scm:git:git://github.com/Skeptick/vk-api-kotlin-client.git")
+                developerConnection.set("scm:git:ssh://github.com:Skeptick/vk-api-kotlin-client.git")
+                url.set("http://github.com/Skeptick/vk-api-kotlin-client/tree/master")
+            }
+        }
+    }
+
+    repositories {
+        maven {
+            name = "bintray"
+            url = uri("https://api.bintray.com/maven/skeptick/maven/vk-api-kotlin-client/;publish=1")
+            credentials {
+                username = findProperty("bintrayUser")?.toString() ?: System.getenv("BINTRAY_USER")
+                password = findProperty("bintrayApiKey")?.toString() ?: System.getenv("BINTRAY_APIKEY")
+            }
+        }
+    }
 }
